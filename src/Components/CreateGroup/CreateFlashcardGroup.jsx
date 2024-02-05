8566aebef9d3013d537e66b9e1e36b4600dd44dd
Manish Chauhan <manishchauhan8222@gmail.com>
2024-02-05 22:31:22 +0530

    update

--- src/Components/CreateGroup/CreateFlashcardGroup.jsx
+++ src/Components/CreateGroup/CreateFlashcardGroup.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import style from "./CreateFlashcardGroup.module.css";
+import { MdDeleteOutline } from "react-icons/md";
 
 export default function CreateFlashcardGroup(props) {
+  const [numCards, setNumCards] = useState(1);
+  const [cards, setCards] = useState([{ id: 1 }]);
   const {
     handleGroupName,
     handleGroupDesc,
@@ -13,7 +16,14 @@ export default function CreateFlashcardGroup(props) {
     handleItemImageUpload,
     itemImage,
   } = props;
-
+  const addMoreHandle = () => {
+    const newCardId = numCards + 1;
+    setCards([...cards, { id: newCardId }]);
+    setNumCards(newCardId);
+  };
+  const removeCard = (cardId) => {
+    setCards(cards.filter((card) => card.id !== cardId));
+  };
   return (
     <form className={style.CreateFlashcard} onSubmit={handleSubmitBtn}>
       <div className={style.CreateFlashcardGroup}>
@@ -60,47 +70,72 @@ export default function CreateFlashcardGroup(props) {
         </div>
       </div>
       <div className={style.CreateFlashcardItem}>
-        <div className={style.CreateFlashcardItemName}>
-          <label htmlFor="cardName">Enter Term *</label>
-          <input
-            type="text"
-            id="cardName"
-            placeholder="Term Name"
-            required
-            onChange={handleCardName}
-          ></input>
-        </div>
-        <div className={style.CreateFlashcardItemName}>
-          <label htmlFor="cardDesc">Enter Defination *</label>
-          <input
-            type="text"
-            id="cardDesc"
-            placeholder="Term Defination"
-            required
-            onChange={handleCardDesc}
-          ></input>
-        </div>
+        {cards.map((card, index) => (
+          <div className={style.CreateFlashcardCard} key={index}>
+            <div className={style.CreateFlashcardItemName}>
+              <div className={style.labelAndDelete}>
+                <label htmlFor={`cardName${card.id}`}>Enter Term *</label>
+                {cards.length > 1 && (
+                  <label
+                    htmlFor={`cardName${card.id}`}
+                    className={style.deleteBtn}
+                    onClick={() => removeCard(card.id)}
+                  >
+                    <MdDeleteOutline />
+                  </label>
+                )}
+              </div>
 
-        <div className={style.itemImage}>
-          <label htmlFor="Card Image">Select Image *</label>
-          <input
-            type="file"
-            accept="image/*"
-            id="itemImage"
-            onChange={handleItemImageUpload}
-          />
-        </div>
-        {itemImage && (
-          <div>
-            <img src={itemImage} alt="Uploaded" style={{ width: "2rem" }}></img>
+              <input
+                type="text"
+                id={`cardName${card.id}`}
+                placeholder="Term Name"
+                required
+                onChange={handleCardName}
+              ></input>
+            </div>
+            <div className={style.CreateFlashcardItemName}>
+              <label htmlFor="cardDesc">Enter Defination *</label>
+              <input
+                type="text"
+                id="cardDesc"
+                placeholder="Term Defination"
+                required
+                onChange={handleCardDesc}
+              ></input>
+            </div>
+
+            <div className={style.itemImage}>
+              <label htmlFor="Card Image">Select Image *</label>
+              <input
+                type="file"
+                accept="image/*"
+                id="itemImage"
+                onChange={handleItemImageUpload}
+              />
+            </div>
+            {itemImage && (
+              <div>
+                <img
+                  src={itemImage}
+                  alt="Uploaded"
+                  style={{ width: "2rem" }}
+                ></img>
+              </div>
+            )}
           </div>
-        )}
+        ))}
+
+        <div className={style.addBtn} onClick={addMoreHandle}>
+          <div className={style.addBtnPlus}>+</div>
+          <div className={style.addBtnText}>Add More</div>
+        </div>
       </div>
       <div className={style.CreateFlashcardBtn}>
         <button
           type="submit"
           onClick={handleSubmitBtn}
-          className={style.createBtn}
+          className={style.creatseBtn}
         >
           Create
         </button>
